80501bd5109f2718bee9b1a5de7c1fae48c18e32
Randall-Segura <yjime01@gmail.com>
2026-02-02 19:17:40 -0600

    Subo cambios

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,19 +22,21 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (input.x != 0)
-        {
-            input.y = 0;    
-        }
+        if (input.x != 0) input.y = 0;
+        
 
         rb.velocity = new Vector2(input.x * moveSpeed * Time.deltaTime, input.y * moveSpeed * Time.deltaTime);
 
 
-        if (rb.velocity != Vector2.zero)
+        if (rb.velocity != Vector2.zero )
         {
             animator.SetFloat("moveX", rb.velocity.x);
             animator.SetFloat("moveY", rb.velocity.y);
+            animator.SetBool("moving", true);
 
+        }
+        else{
+            animator.SetBool("moving", false);
         }
 
 
@@ -43,8 +45,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Vertical");
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
 
     }
 }
